91b7a1712e47d5255a57d41b231da16b4ca9f78b
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-03-25 16:57:06 +0700

    update materi

---
+++ Namespace.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "Data/Conflict.php";
+require_once "Data/Helper.php";
+
+$conflict1 = new Data\One\Conflict();
+$conflict2 = new Data\Two\Conflict();
+
+echo Helper\APPLICATION . PHP_EOL;
+
+Helper\helpMe();
