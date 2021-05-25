4e6872b62e65054ced5ec9937a93bb582a214421
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-05-25 21:13:29 +0700

    tambah materi interface

---
+++ Produk-Interface.php
@@ -0,0 +1,171 @@
+<?php
+
+interface InfoProduk
+{
+    public function getInfoProduk();
+}
+
+abstract class Produk
+{
+    // Property => mereprensentasikan data/keadaan dari sebuah objek
+    protected  $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0,
+        $diskon = 0;
+
+
+    // Contruct => method special yang dimiliki oleh Class
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    // Method => merepresentasikan perilaku dari sebuah objek
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    abstract public function getInfo();
+
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+}
+
+class Komik extends Produk implements InfoProduk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+
+    public function getInfoProduk() // implementasi dari method abstract
+    {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+class Game extends Produk implements InfoProduk
+{
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+
+    public function getInfoProduk() // implementasi dari method abstract
+    {
+        $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    public $daftarProduk = array();
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak()
+    {
+        $str = "DAFTAR PRODUK : " . PHP_EOL;
+
+        foreach ($this->daftarProduk as $p) {
+            $str .= "- {$p->getInfoProduk()}" . PHP_EOL;
+        }
+
+        return $str;
+    }
+}
+
+
+// Dengan menggunakan contruct instance nya menjadi seperti ini
+$produk1 = new Komik("Naritik Naoto", "Martin", "Sempurna Street", 11000000, 100);
+$produk2 = new Game("Game cacing cacing", "Iska", "Langkat Group", 112000000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
+
+// Kenapa menggunakan kelas abstract?
+// 1. Mempresentasikan ide atau konsep dasar
+// 2. Composition over Inheritance => Melakukan komposisi (abstraksi atau interface) dibandingkan melakukan inheritance
+// 3. Salah satu cara menerapkan Polimorphism
+// 4. Sentralisasi logic
